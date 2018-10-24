586fb91f72bc6879b739909e8ca30400614a3220
AshGale <ashgale17@yahoo.co.uk>
2018-10-24 09:37:05 +0100

    Added Interfaces for models. closes #5.

---
+++ pet-clinic-data/src/main/java/com/web/boot/gurupetclinic/service/OwnerService.java
@@ -0,0 +1,16 @@
+package com.web.boot.gurupetclinic.service;
+
+import com.web.boot.gurupetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/web/boot/gurupetclinic/service/VetService.java
@@ -0,0 +1,14 @@
+package com.web.boot.gurupetclinic.service;
+
+import com.web.boot.gurupetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}

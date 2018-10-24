3b67cde3640b2f8584e95291512cbf84e67ae131
AshGale <ashgale17@yahoo.co.uk>
2018-10-24 17:56:17 +0100

    Added index page with thymleaf . closes #w.

---
+++ pet-clinic-web/src/main/java/com/web/boot/gurupetclinic/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.web.boot.gurupetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"/","","index","index.html"})
+    public String index(){
+        return "index";
+    }
+}

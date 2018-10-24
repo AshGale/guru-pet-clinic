e26f3ec2078e3cadaf325ca34ea8b6f9022fa8c6
AshGale <ashgale17@yahoo.co.uk>
2018-10-24 12:40:37 +0100

    Implemented map based services . closes #4.

---
+++ pet-clinic-data/src/main/java/com/web/boot/gurupetclinic/service/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package com.web.boot.gurupetclinic.service.map;
+
+import com.web.boot.gurupetclinic.model.Vet;
+import com.web.boot.gurupetclinic.service.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}

977838f68fd733fc92fa2b6868fbd9b16f833da7
AshGale <ashgale17@yahoo.co.uk>
2018-10-24 12:08:38 +0100

    Added Common Crud interface. closes #11.

--- pet-clinic-data/src/main/java/com/web/boot/gurupetclinic/service/PetService.java
+++ pet-clinic-data/src/main/java/com/web/boot/gurupetclinic/service/PetService.java
@@ -2,13 +2,6 @@ package com.web.boot.gurupetclinic.service;
 
 import com.web.boot.gurupetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService <Pet, Long>{
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/com/web/boot/gurupetclinic/service/VetService.java
+++ pet-clinic-data/src/main/java/com/web/boot/gurupetclinic/service/VetService.java
@@ -2,13 +2,6 @@ package com.web.boot.gurupetclinic.service;
 
 import com.web.boot.gurupetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService <Vet, Long>{
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }

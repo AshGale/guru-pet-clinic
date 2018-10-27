035d72fdcc1e1dd12eca8098d787eabb5c6b3768
AshGale <ashgale17@yahoo.co.uk>
2018-10-27 16:26:18 +0100

    Refactored id generation for id's in abstract calls. closes #23.

--- pet-clinic-web/src/main/java/com/web/boot/gurupetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/web/boot/gurupetclinic/bootstrap/DataLoader.java
@@ -24,14 +24,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Arron");
         owner1.setLastName("Arnold");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Ben");
         owner2.setLastName("Brunswic");
 
@@ -40,14 +38,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Carol");
         vet1.setLastName("Carlson");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Deen");
         vet2.setLastName("Deans");
 
